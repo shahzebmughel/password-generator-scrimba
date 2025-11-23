d7c02eea0aa4116338092034ce418f9766170bdc
shahzebmughel <shahzebmughel@hotmail.com>
2025-11-24 00:28:19 +0100

    Base Goal done Stretch goals next thats why v1

--- v1/index.js
+++ v1/index.js
@@ -21,18 +21,32 @@ const COMBINED_EVERYTHING = [...letters, ...numbers, ...symbols];
 const COMBINED_NUMBERS = [...letters, ...numbers];
 const COMBINED_SYMBOLS = [...letters, ...symbols];
 
-
 const generatePasswordBtn = document.getElementById("generate-password-btn");
-const passwordFieldOne = document.getElementById("password-field-one");
-const passwordFieldTwo = document.getElementById("password-field-two");
+const passwordOneField = document.getElementById("password-field-one");
+const passwordTwoField = document.getElementById("password-field-two");
+
 
 // Make a for loop and use a Random number that accesses the array from letters numbers and symbols for each position
 // that prints out a password that is 15 chars long
 
 function generatePassword(passwordLength, includeNumbers, includeSymbols) {
+    let password1 = "";
+    let password2 = "";
+
+    for (let i = 0; i < passwordLength; i++) {
+        let randomValue1 = Math.floor(Math.random() * COMBINED_EVERYTHING.length);
+        let randomValue2 = Math.floor(Math.random() * COMBINED_EVERYTHING.length);
+
+        password1 += COMBINED_EVERYTHING[randomValue1];
+        password2 += COMBINED_EVERYTHING[randomValue2];
+
+    }
+    passwordOneField.textContent = password1;
+    passwordTwoField.textContent = password2;
 
 }
 
 generatePasswordBtn.addEventListener("click", function () {
     console.log("button clicked!")
+    generatePassword(15, false, false);
 })
